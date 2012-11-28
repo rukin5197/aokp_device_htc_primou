1bdf12ba75f22c50824cf79dfbd2899d2e9a87d9
maxwen <max.weninger@gmail.com>
2012-11-28 02:35:42 +0100

    fixed wrong path

--- device.mk
+++ device.mk
@@ -104,8 +104,8 @@ PRODUCT_COPY_FILES += \
 
 # bluez config
 PRODUCT_COPY_FILES += \
-    device/htc/primou/prebuilt/etc/dbus.conf:/etc/dbus.conf \
-    device/htc/primou/prebuilt/etc/dbus-1/system-services/org.bluez.service:/etc/dbus-1/system-services/org.bluez.service
+    device/htc/primou/prebuilt/etc/dbus.conf:system/etc/dbus.conf \
+    device/htc/primou/prebuilt/etc/dbus-1/system-services/org.bluez.service:system/etc/dbus-1/system-services/org.bluez.service
 
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
